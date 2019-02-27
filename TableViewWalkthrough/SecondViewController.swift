386b5d6a38428823bed478fb636be6b51423c159
Augustine Kwong <augustinekwong@gmail.com>
2019-02-27 13:55:59 -0800

    Initial Commit

---
+++ TableViewWalkthrough/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TableViewWalkthrough
+//
+//  Created by Augustine Kwong on 2019-02-27.
+//  Copyright © 2019 Augustine Kwong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
